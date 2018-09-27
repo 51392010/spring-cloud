0a28efa50ec55f8722491c330cf01d220dec2596
123 <123@example.com>
2018-09-27 13:27:10 +0800

    commit version0.1

---
+++ microservice-consumer-user/src/main/java/com/xinjump/MicroserviceConsumerUserApplication.java
@@ -0,0 +1,12 @@
+package com.xinjump;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroserviceConsumerUserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroserviceConsumerUserApplication.class, args);
+	}
+}
